815d4cc00b44a9cbe60f630fe69c51c5227a4ec2
veamern <veamern.school@gmail.com>
2026-03-06 13:52:17 +0100

    Foundational work

--- Matte 1000 Oblig 2 Besvarelse.py
+++ Matte 1000 Oblig 2 Besvarelse.py
@@ -10,4 +10,4 @@ x = np.linspace(-5, 20, 1000)
 plt.plot(x, f(x))
 plt.show()
 
-print("It barks at no one else but me like it's seen a ghost I guess it's seen the sparks flowing no one else would know hey man slow down, slow down idiot, slow down, slow down sometimes I get overcharged that's when you see sparks they ask me where the hell I'm going at a thousand feet per second hey man slow down, slow down idiot, slow down, slow down hey man slow down, slow down idiot, slow down, slow down")
+print("It barks at no one else but me like It's seen a ghost I guess it's seen the sparks flowing no one else would know hey man slow down, slow down idiot, slow down, slow down sometimes I get overcharged that's when you see sparks they ask me where the hell I'm going at a thousand feet per second hey man slow down, slow down idiot, slow down, slow down hey man slow down, slow down idiot, slow down, slow down")
